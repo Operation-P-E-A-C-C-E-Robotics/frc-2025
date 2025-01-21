2eae8cfa522ffda0f0d54c33a77e23dc957bfcb4
frc-3461 <programming@peacce.org>
2025-01-20 19:04:42 -0500

    Did stuff

--- src/main/java/frc/robot/subsystems/Chute.java
+++ src/main/java/frc/robot/subsystems/Chute.java
@@ -4,14 +4,19 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 public class Chute extends SubsystemBase {
   /** Creates a new Chute. */
   //2 BAG motors (1 talon fx) + a solenoid or servo, and we should set up (commented out) code for the possible second one, using master-slave system
+  
+  public static TalonFX rightChuteMotor;
+
   public Chute()
   {
-    
+     
   }
 
   @Override
