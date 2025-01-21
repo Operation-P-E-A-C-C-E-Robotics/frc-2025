aa9e3699ddc033ff085fee278bc8a335c0152525
ParKouRrr <cloverkitty8@gmail.com>
2025-01-20 19:32:02 -0500

    idk

--- src/main/java/frc/robot/subsystems/Sushi.java
+++ src/main/java/frc/robot/subsystems/Sushi.java
@@ -4,27 +4,29 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.function.BooleanSupplier;
+
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class Sushi extends SubsystemBase {
   private final TalonFX spinner = new TalonFX(99);
-
+  private BooleanSupplier bool;
+    
   /** Creates a new Sushi. */
   //Set up literally one falcon 500, that (theoretically) triggers when you press a button/trigger. You can do this!!!
   // Backwards AND forwards
   public Sushi() {}
-
-  public Command soy(Boolean bool) {
-    spinner.set(0);
-    return runOnce(
-      () -> {}
-    );
+    
+  public Sushi soy(BooleanSupplier bool) {
+    this.bool = bool;
+    return this;
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    if (bool) {
+      spinner.set(0);
+    }
   }
 }
