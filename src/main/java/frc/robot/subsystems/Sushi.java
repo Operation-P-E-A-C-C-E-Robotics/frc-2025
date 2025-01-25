09f61dc73b1b26693bc363177dbdd1671414a667
frc-3461 <programming@peacce.org>
2025-01-24 19:10:03 -0500

    Save file

--- src/main/java/frc/robot/subsystems/Sushi.java
+++ src/main/java/frc/robot/subsystems/Sushi.java
@@ -14,6 +14,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.IMotorController;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.sensors.CANCoderStickyFaults;
 
 public class Sushi extends SubsystemBase {
 
