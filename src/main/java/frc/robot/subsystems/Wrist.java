9ce6d06af921c01d7a5e535f2260688978345770
frc-3461 <programming@peacce.org>
2025-03-22 11:20:45 -0400

    poggers

--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -110,7 +110,7 @@ public class Wrist extends SubsystemBase {
 
     //setpoints always at bottom of subsytem
     public enum WristSetpoints {
-        REST(Rotation2d.fromDegrees(12)),
+        REST(Rotation2d.fromDegrees(8.5)),
         L1(Rotation2d.fromDegrees(0)),
         L2L3(Rotation2d.fromDegrees(25)),
         L4(Rotation2d.fromDegrees(55));
