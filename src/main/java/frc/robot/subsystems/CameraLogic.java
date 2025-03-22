d38e733bc1a6517ef82877497db73362331af52b
frc-3461 <programming@peacce.org>
2025-03-22 11:21:03 -0400

    v2

--- src/main/java/frc/robot/subsystems/CameraLogic.java
+++ src/main/java/frc/robot/subsystems/CameraLogic.java
@@ -5,7 +5,9 @@ import org.photonvision.EstimatedRobotPose;
 
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.vision.ApriltagCamera.VisionResults;
 import frc.robot.RobotContainer;
+import edu.wpi.first.apriltag.AprilTag.*;
 
 
 public class CameraLogic extends SubsystemBase 
@@ -13,19 +15,18 @@ public class CameraLogic extends SubsystemBase
     
     public CameraLogic() {}
 
-    // @Override
-    // public void periodic()
-    // {
-    // final Optional<EstimatedRobotPose> optionalEstimatedPoseRight = RobotContainer.photonPoseEstimatorRight.update(RobotContainer.camera.getLatestResult());
-    // final Optional<EstimatedRobotPose> optionalEstimatedPoseLeft = RobotContainer.photonPoseEstimatorLeft.update(RobotContainer.camera.getLatestResult());
-    // final SwerveDrivePoseEstimator swervePoseEstimator;
-    //   if
-    //   (optionalEstimatedPoseLeft.isPresent()) {
-    //     //final EstimatedRobotPose estimatedPoseLeft = optionalEstimatedPoseLeft.get();   
-        
-        
-    //     //swervePoseEstimator.addVisionMeasurement(estimatedPoseLeft.toString(), estimatedPoseLeft.timestampSeconds);
-    //   }
-    //   final EstimatedRobotPose estimatedPoseRight = optionalEstimatedPoseRight.get();
-    // }
+    @Override
+    public void periodic()
+    {
+    final Optional<EstimatedRobotPose> optionalEstimatedPoseRight = RobotContainer.photonPoseEstimatorRight.update(RobotContainer.camera.getLatestResult());
+    final Optional<EstimatedRobotPose> optionalEstimatedPoseLeft = RobotContainer.photonPoseEstimatorLeft.update(RobotContainer.camera.getLatestResult());
+    VisionResults vizzies;
+    final SwerveDrivePoseEstimator swervePoseEstimator;
+      if(optionalEstimatedPoseLeft.isPresent()) 
+      {
+        final EstimatedRobotPose estimatedPoseLeft = optionalEstimatedPoseLeft.get();   
+        //swervePoseEstimator.addVisionMeasurement(estimatedPoseLeft., estimatedPoseLeft.timestampSeconds);
+      }
+      final EstimatedRobotPose estimatedPoseRight = optionalEstimatedPoseRight.get();
+    }
 }
